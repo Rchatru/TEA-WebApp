095493952106e2213ac8f01906c8e3dd1ced5a2a
Roberto Chávez Trujillo <chaveztrujilloroberto@gmail.com>
2022-06-04 06:15:52 +0100

    Name change

---
+++ Home.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_config(
+     page_title="TEA WebApp",
+     page_icon="👀",
+     menu_items={
+         'Get Help': 'https://github.com/Rchatru/TEA-WebApp/',
+         'Report a bug': "https://github.com/Rchatru/TEA-WebApp/issues",
+         'About': "# TEA WebApp. Roberto Chávez Trujillo."
+     }
+ )
+
+st.title('TEA WebApp')
+
+st.markdown('''
+WebApp construida con Python y Streamlit diseñada para proporcionar una interacción más amigable con los resultados de la investigación del presente doctorado orientado al diagnóstico del Trastorno del Espectro Autista a partir de datos de la dinámica ocular de los participantes obtenida mediante eye-tracker.
+
+La aplicación se encuentra dividida en diferentes subpáginas, cada una con un fin distinto bien diferenciado:
+
+- En primer lugar se tiene la pantalla de inicio "Home", que contiene una introducción básica y, desde la que se puede acceder al resto de ellas.
+- En segundo término se encuentra una página en la que se pueden aportar más datos de entrenamiento para llevar a cabo un reentrenamiento del algoritmo.
+- Por último, en la tercera se muestran una serie de resultados, así como se tiene la posibilidad de introducir los datos de un individuo en concreto para llevar a cabo su diagnóstico por parte del modelo.
+''')
