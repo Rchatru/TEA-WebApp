e2b93e153db25ab60154a0a618db48ed3fda687a
Roberto Chávez Trujillo <chaveztrujilloroberto@gmail.com>
2022-06-04 05:23:05 +0100

    Estructura de paginas inicial

---
+++ 🏠 Home.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_config(
+     page_title="TEA WebApp",
+     page_icon="👀",
+     layout="",
+     initial_sidebar_state="",
+     menu_items={
+         'Get Help': 'https://www.extremelycoolapp.com/help',
+         'Report a bug': "https://www.extremelycoolapp.com/bug",
+         'About': "# This is a header. This is an *extremely* cool app!"
+     }
+ )
