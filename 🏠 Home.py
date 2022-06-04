e30b559ea900c217194ca93aea43e87ea4d10328
Roberto Chávez Trujillo <chaveztrujilloroberto@gmail.com>
2022-06-04 06:06:03 +0100

    Descripción

--- 🏠 Home.py
+++ 🏠 Home.py
@@ -14,3 +14,12 @@ st.set_page_config(
 
 st.title('TEA WebApp')
 
+st.markdown('''
+WebApp construida con Python y Streamlit diseñada para proporcionar una interacción más amigable con los resultados de la investigación del presente doctorado orientado al diagnóstico del Trastorno del Espectro Autista a partir de datos de la dinámica ocular de los participantes obtenida mediante eye-tracker.
+
+La aplicación se encuentra dividida en diferentes subpáginas, cada una con un fin distinto bien diferenciado:
+
+- En primer lugar se tiene la pantalla de inicio "Home", que contiene una introducción básica y, desde la que se puede acceder al resto de ellas.
+- En segundo término se encuentra una página en la que se pueden aportar más datos de entrenamiento para llevar a cabo un reentrenamiento del algoritmo.
+- Por último, en la tercera se muestran una serie de resultados, así como se tiene la posibilidad de introducir los datos de un individuo en concreto para llevar a cabo su diagnóstico por parte del modelo.
+''')
