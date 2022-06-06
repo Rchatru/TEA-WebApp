b65c8bce351980d4c9aff2c13fe309b02ba14bc0
Roberto Chávez Trujillo <chaveztrujilloroberto@gmail.com>
2022-06-06 04:52:57 +0100

    Añadida función info y upload data

--- functions.py
+++ functions.py
@@ -1,9 +1,19 @@
 import streamlit as st
+import pandas as pd
 import pickle
+import io
 
 @st.cache
 def convert_df(df):
    return df.to_csv().encode('utf-8')
+
+@st.cache
+def df_info(df):
+   # For showing dataframe info
+   buffer = io.StringIO()
+   df.info(verbose=True,buf=buffer)
+   s = buffer.getvalue() 
+   return s
 
 @st.cache
 def predict(df):
@@ -13,9 +23,13 @@ def predict(df):
    X = X.loc[:, BestVars]
    model = pickle.load(open('/static/XGBClassifier.sav', 'rb'))
    result = model.predict(X)
-
    return result
 
+@st.cache
+def upload_test_data():
+   # Open stored .csv file at static folder an convert to dataframe
+   df = pd.read_csv("/static/test_data.csv",sep=';') 
+   return df
 
 # model = XGBClassifier()
 # model.load_model("XGBClassifier.json")
