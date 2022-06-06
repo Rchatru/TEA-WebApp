39a841690e85728c7eefbeaba9109c43e232e91e
Roberto Chávez Trujillo <chaveztrujilloroberto@gmail.com>
2022-06-06 04:54:54 +0100

    Mejor carga de datos, tabla de ejemplo y nota

--- pages/3_✅_Predict.py
+++ pages/3_✅_Predict.py
@@ -1,6 +1,8 @@
+import click
 import streamlit as st
 import pandas as pd
 import numpy as np
+import io
 import pickle
 # from xgboost import XGBClassifier
 from functions import *
@@ -25,37 +27,53 @@ En esta pantalla se puede consultar la predicción para un individuo o grupos en
 st.markdown('''
 Aunque la página se encarga de adecuar los datos a la forma requerida, se recomienda que el usuario introduzca los datos de entrada en 
 el formato CSV siguiendo el siguiente esquema de columnas: 
-esc = 6
-BestVars = ['FixationPointX_(MCSpx)','FixationPointY_(MCSpx)','Fixation','Saccade','Unclassified']
-fichero normalizado (std)
-validacion = model.predict(X_val)
 ''')
 
+
+
 st.table(
-pd.DataFrame(10*np.random.randn(3, 5),
-columns=['A', 'B', 'C', 'D', 'E'])
+pd.DataFrame([[750,326,1,0,0],[0,0,0,1,0],[120,562,0,0,1]],
+columns=['FixationPointX_(MCSpx)', 'FixationPointY_(MCSpx)', 'Fixation', 'Saccade', 'Unclassified'])
 )
 
-
+st.caption('''
+Nota: El modelo actual desarrollado sólo precisa los datos de la escena 6 y se basa únicamente en las variables 
+`['FixationPointX_(MCSpx)','FixationPointY_(MCSpx)','Fixation','Saccade','Unclassified']`.
+También se espera que el fichero de datos se encuentre normalizado.
+''')
 
  # Upload individual's data to be tested
 with st.sidebar.header('1. Upload your data file'):
     input = st.sidebar.file_uploader("Upload your input file", type=["csv"])
+    
 
 
 if input is not None:  
 
-    st.markdown('''
+    st.subheader('''
     Vista previa de los datos de entrada:
     ''')
     df = pd.read_csv(input,sep=';')
     st.dataframe(df.head())
 
+    with st.expander("See dataset debug info"):
+        st.text(df_info(df))
     
-
-
-
     csv = convert_df(df)
     with st.sidebar:
         st.header('2. Download results file')
         st.download_button('Download file', csv, 'results.csv', 'text/csv',key='download-csv')
+
+else:
+    with st.sidebar:
+        placeholder = st.empty()
+        st.sidebar.caption('<p style="color:#484a55;">Cargar fichero con los individuos de test</p>', unsafe_allow_html=True)
+        if not st.sidebar.button("Test Dataset"):
+            placeholder.info("No se ha cargado ningún fichero. Seleccione uno o escoja el dataset de test disponible.")
+            
+            
+
+        
+# if not st.checkbox("Hide dataframe"):
+#     df = pd.DataFrame(range(0, 10))
+#     placeholder.dataframe(df)
