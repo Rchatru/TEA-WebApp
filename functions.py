00685ab3a7e12b47147d4bbae1a3e5aaaca92908
Roberto Chávez Trujillo <chaveztrujilloroberto@gmail.com>
2022-06-08 05:46:53 +0100

    sin cache

--- functions.py
+++ functions.py
@@ -6,11 +6,11 @@ import pickle
 import io
 import sys
 
-@st.cache
+# @st.cache
 def convert_df(df):
    return df.to_csv().encode('utf-8')
 
-@st.cache
+# @st.cache
 def df_info(df):
    # For showing dataframe info
    buffer = io.StringIO()
@@ -20,56 +20,56 @@ def df_info(df):
 
 vars = ['FixationPointX_(MCSpx)','FixationPointY_(MCSpx)','Fixation','Saccade','Unclassified']
 
-class stdScaler(TransformerMixin, BaseEstimator):
+# class stdScaler(TransformerMixin, BaseEstimator):
 
-    def fit(self, X, y=None):
-        self.means_ = X.mean(axis=0)
-        self.std_dev_ = X.std(axis=0)
-        return self
+#     def fit(self, X, y=None):
+#         self.means_ = X.mean(axis=0)
+#         self.std_dev_ = X.std(axis=0)
+#         return self
 
-    def transform(self, X, y=None):
-        return (X - self.means_[:X.shape[1]]) / self.std_dev_[:X.shape[1]]
+#     def transform(self, X, y=None):
+#         return (X - self.means_[:X.shape[1]]) / self.std_dev_[:X.shape[1]]
 
 
-def check_df(df_in):
-   # En primer lugar ajusta el nombre de las columnas a la forma requerida
-   df_in.rename(columns=lambda x: x.replace(" ", "_"), inplace=True)
-   cols = df_in.columns.tolist()
+# def check_df(df_in):
+#    # En primer lugar ajusta el nombre de las columnas a la forma requerida
+#    df_in.rename(columns=lambda x: x.replace(" ", "_"), inplace=True)
+#    cols = df_in.columns.tolist()
 
-   # Compara los elementos de la lista de variables necesarias (vars) con las del archivo introducido (cols).
-   if set(vars).issubset(set(cols)):
-      if len(vars) == len(cols):
-         df = df_in
-      else:
-         message = 'Las variables del archivo de entrada no coinciden con las esperadas. Se eliminarán las no necesarias.'
-         st.info(message)
-         df = df_in.drop(columns=set(cols) - set(vars))
-   else:
-      st.error("El archivo introducido no tiene todas las variables necesarias.")
-      return False
-      #sys.exit()
+#    # Compara los elementos de la lista de variables necesarias (vars) con las del archivo introducido (cols).
+#    if set(vars).issubset(set(cols)):
+#       if len(vars) == len(cols):
+#          df = df_in
+#       else:
+#          message = 'Las variables del archivo de entrada no coinciden con las esperadas. Se eliminarán las no necesarias.'
+#          st.info(message)
+#          df = df_in.drop(columns=set(cols) - set(vars))
+#    else:
+#       st.error("El archivo introducido no tiene todas las variables necesarias.")
+#       return False
+#       #sys.exit()
       
-   df.fillna(0, inplace=True)
+#    df.fillna(0, inplace=True)
 
-   # Cálculo de los cuartiles y rango IQR
-   Q1 = df[vars].quantile(0.25)
-   Q3 = df[vars].quantile(0.75)
-   IQR = Q3 - Q1
+#    # Cálculo de los cuartiles y rango IQR
+#    Q1 = df[vars].quantile(0.25)
+#    Q3 = df[vars].quantile(0.75)
+#    IQR = Q3 - Q1
 
-   # Límites superior e inferior para el cálculo de los outliers
-   k = 3
-   l_sup = Q3 + k*IQR
-   l_inf = Q1 - k*IQR
+#    # Límites superior e inferior para el cálculo de los outliers
+#    k = 3
+#    l_sup = Q3 + k*IQR
+#    l_inf = Q1 - k*IQR
 
-   # Se eliminan los outliers que se encuentren por encima del límite superior o por debajo del límite inferior
-   fix_X = (encoded_train.loc[(encoded_train['FixationPointX_(MCSpx)'] >= l_sup['FixationPointX_(MCSpx)']) | (encoded_train['FixationPointX_(MCSpx)'] <= l_inf['FixationPointX_(MCSpx)'])])
-   limp_train = encoded_train[~((encoded_train < m_inf) | (encoded_train > m_sup)).any(axis=1)]
+#    # Se eliminan los outliers que se encuentren por encima del límite superior o por debajo del límite inferior
+#    fix_X = (encoded_train.loc[(encoded_train['FixationPointX_(MCSpx)'] >= l_sup['FixationPointX_(MCSpx)']) | (encoded_train['FixationPointX_(MCSpx)'] <= l_inf['FixationPointX_(MCSpx)'])])
+#    limp_train = encoded_train[~((encoded_train < m_inf) | (encoded_train > m_sup)).any(axis=1)]
 
    
-   stdscaler = stdScaler()
-   df[vars] = stdscaler.fit_transform(df[vars])
+#    stdscaler = stdScaler()
+#    df[vars] = stdscaler.fit_transform(df[vars])
 
-   return df
+#    return df
 
 # @st.cache
 def predict(df):
