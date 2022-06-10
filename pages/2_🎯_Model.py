c19886e3a72e781d9488d304b58f7a35f304f187
Roberto Chávez Trujillo <chaveztrujilloroberto@gmail.com>
2022-06-10 05:39:55 +0100

    Añadidas imágenes, widget subir modelo y métricas id

--- pages/2_🎯_Model.py
+++ pages/2_🎯_Model.py
@@ -21,11 +21,32 @@ datos de entremaniento en caso de disponer de ellos, para de este modo
 reentrenar el modelo.
  ''')
 
+# Hack para quitar las flechas del widget
+st.write(
+    """
+    <style>
+    [data-testid="stMetricDelta"] svg {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 
-col1, col2, col3 = st.columns(3)
-col1.metric("Temperature", "70 °F", "1.2 °F")
-col2.metric("Wind", "9 mph", "-8%")
-col3.metric("Humidity", "86%", "4%")
+col1, col2, col3, col4 = st.columns(4)
+col1.metric("Individuo 05c", "72.80%", "Control")
+col2.metric("Individuo 09c", "75.89%", "Control")
+col3.metric("Individuo 07p", "72.67%", "TEA",delta_color='inverse')
+col4.metric("Individuo 08p", "91.16%", "TEA",delta_color='inverse')
 
 with st.sidebar.header('1. Upload your new training data'):
     input = st.sidebar.file_uploader("Upload your file", type=["csv"])
+
+st.sidebar.subheader("Or")
+
+with st.sidebar.header('2. Upload a new model file'):
+    input = st.sidebar.file_uploader("Upload your file", type=[".json", ".bin", ".model"])
+
+
+st.image('static/images/resultados.png',caption='Gráficas de entrenamiento', use_column_width=True)
+st.image('static/images/confusion_func.png',caption='Matriz de confusión normalizada', use_column_width=True)
