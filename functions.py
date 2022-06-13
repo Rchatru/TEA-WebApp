91f8c5a1e05b3bb8ac469e54a7e69be93e19af5f
Roberto Chávez Trujillo <chaveztrujilloroberto@gmail.com>
2022-06-13 03:37:14 +0100

    Documentación funciones. Añadido metrics, crosstab y modificado predict

--- functions.py
+++ functions.py
@@ -3,19 +3,35 @@ from sklearn.base import TransformerMixin
 from sklearn.base import BaseEstimator
 from sklearn.preprocessing import LabelEncoder
 import pandas as pd
-import pickle
+# import pickle
 import io
 import sys
+import time
 
 import xgboost
 
-# @st.cache
+
 def convert_df(df):
+   """
+   Esta función permite convertir un dataframe en un archivo .csv listo para descargar.
+   input:
+      df: dataframe a convertir.
+   output:
+      archivo .csv
+   """
+
    return df.to_csv().encode('utf-8')
 
-# @st.cache
+
 def df_info(df):
-   # For showing dataframe info
+   """
+   Esta función captura el textI/O enviado directamente a consola por métodos como df.info().
+   input:
+      df: dataframe del que se desea mostrar los detalles.
+   output:
+      s: string con la información del dataframe.
+   """
+   # "Truco" necesario para mostrar cosas como df.info() en la página.
    buffer = io.StringIO()
    df.info(verbose=True,buf=buffer)
    s = buffer.getvalue() 
@@ -34,6 +50,10 @@ vars = ['FixationPointX_(MCSpx)','FixationPointY_(MCSpx)','Fixation','Saccade','
 #         return (X - self.means_[:X.shape[1]]) / self.std_dev_[:X.shape[1]]
 
 
+
+# La selección de escena debe estar aqui
+# esc = 6
+# X = df.loc[df['escena' + str(esc)] == 1]
 # def check_df(df_in):
 #    # En primer lugar ajusta el nombre de las columnas a la forma requerida
 #    df_in.rename(columns=lambda x: x.replace(" ", "_"), inplace=True)
@@ -74,11 +94,18 @@ vars = ['FixationPointX_(MCSpx)','FixationPointY_(MCSpx)','Fixation','Saccade','
 
 #    return df
 
-# @st.cache
+
+# Es necesario añadir esta función al cache para que no se ejecute la predicción cada vez que se actualiza la página.
+@st.experimental_memo(suppress_st_warning=True)
 def predict(df):
-   # La selección de escena no debe estar aqui
-   # esc = 6
-   # X = df.loc[df['escena' + str(esc)] == 1]
+   """
+   Esta función permite realizar la clasificación de los datos en base al modelo XGBoost importado.
+   input:
+      df: dataframe con los datos a predecir. Se espera que el dataset esté previamente procesado con la función check_df() o
+      importado con upload_test_data(). Debe contener las variables TEA e id. #TODO: #3 Hay que tener en cuenta que no siempre se podrá disponer de la variable objetivo, TEA.
+   output:
+      df_result: dataframe original con una nueva columna con la clase predicha, 'Pred'.
+   """   
    
    X = df.loc[:, vars]
    Y = df.loc[:,['TEA', 'id']]
@@ -86,18 +113,98 @@ def predict(df):
    model = xgboost.XGBClassifier()
    model.load_model('static/XGBClassifier.bin')
    # Importante para evitar bug de XGBoost https://github.com/dmlc/xgboost/issues/2073
-   # FIXME: Investigar cual es el valor correcto 0 o 1
    model._le = LabelEncoder().fit([0,1])
 
    result = model.predict(X)
-   return result
+   result = pd.DataFrame(result, columns=['Pred'])
+   # Unir de nuevo los dataframes X, Y y result en uno solo
+   df_result = pd.concat([X, Y, result], axis=1)
+   
+   # Se añade la barra de progreso en la función para evitar que se muestre cada vez que se actualiza la página.
+   my_bar = st.progress(0)
+   for progress in range(100):
+      time.sleep(0.01)
+      my_bar.progress(progress + 1)
+   my_bar.empty()
+
+   return df_result
+
+def crosstab(df):
+   """
+   Función que muestra los resultados de la predicción.
+   input: 
+      df: Dataframe con los resultados de la predicción obtenido de la función predict
+   output:
+      crosstab: Dataframe con el número de muestras asignadas a cada clase según individuo.
+   """
+   
+   # Para cada 'id' se muestra la cuenta de las muestras clasificadas como '0' y como '1'
+   crosstab = pd.crosstab(df.id, df.Pred).rename(columns={0: 'Control', 1: 'TEA'})
+   # Se añade columna con el total de muestras por individuo.
+   crosstab['Muestras'] = crosstab['Control'] + crosstab['TEA']
+   
+   return crosstab
 
 
-# Por alguna razón da resultados extraños si se añade cache a esta función
+
+def metrics(df,ind,umbral):
+   """
+   Esta función permite realizar el cálculo del porcentaje de acierto para cada uno de los individuos y pasar este y otros datos auxiliares 
+   al widget encargado de mostrar los resultados.
+   input:
+      df: dataframe obtenido de la función predict (debe contener las columnas 'id' y 'Pred').
+      ind: individuo a calcular los resultados.
+      umbral: umbral de confianza para la clasificación (obtenido del widget slider).
+   output:
+      percent: porcentaje de muestras sobre el total clasificadas correctamente.
+      tipo: clase en la que se ha clasificado el individuo (TEA/Control).
+      color: variable para el ajuste de color del widget dependiendo de la clase del individuo.
+   """
+
+   umbral = umbral/100
+   total = df.loc[df['id'] == ind]['Pred'].count()
+   sum1 = df.loc[df['id'] == ind]['Pred'].eq(1).sum()
+   sum0 = df.loc[df['id'] == ind]['Pred'].eq(0).sum()
+   if sum1 >= total*umbral:
+      percent = '{:.2f}'.format(sum1/total*100) + '%'
+      tipo = 'TEA'
+      color = 'inverse'
+   elif sum0 >= total*umbral:
+      percent = '{:.2f}'.format(sum0/total*100) + '%'
+      tipo = 'Control'
+      color = 'normal'
+   elif sum1 < total*umbral and sum0 < total*umbral and sum1 > sum0:
+      percent = '{:.2f}'.format(sum1/total*100) + '%'
+      tipo = 'Posible TEA'
+      color = 'off'
+   elif sum1 < total*umbral and sum0 < total*umbral and sum0 > sum1:
+      percent = '{:.2f}'.format(sum0/total*100) + '%'
+      tipo = 'Posible Control'
+      color = 'off'
+   else:
+      percent = 0
+      tipo = 'No definido'
+      color = 'off'
+
+   return percent,tipo,color
+
+
+
 def upload_test_data():
+   """
+   Esta función permite subir un archivo de testeo para realizar la clasificación. Se ha escogido un dataset ya procesado en el 
+   archivo Principal_sript.py (One Hot Encoding, FillNa, outliers, estandarización, etc.)
+   input:
+      None
+   output:
+      df: dataframe con los datos de test.
+   """
+
    # Open stored .csv file at static folder an convert to dataframe
    df = pd.read_csv("static/test_data.csv",sep=';') 
    cols = df.columns.to_list()
+   # Es necesario disponer una variable que contenga todos los id de los individuos
    df['id'] = pd.get_dummies(df[['07p', '08p', '05c', '09c']]).idxmax(1)
+   # Se eliminan todas las demás menos id y TEA
    df = df.drop(columns=set(cols) - set(vars+['id','TEA']))
    return df
