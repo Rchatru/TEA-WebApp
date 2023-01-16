8af705a4e336f5b31c99d38c9d49b6ba17954fb9
Roberto Chávez Trujillo <chaveztrujilloroberto@gmail.com>
2023-01-16 10:58:48 +0000

    fixed show_s3_content for show_structure

--- functions.py
+++ functions.py
@@ -151,10 +151,10 @@ def show_file_structure(folder):
    for file in all_files:
       if file[-1] == '/':
          # st.text('└── ' + file)
-         text = text + ('└── ' + file + '\n')
+         text = text + ('└── ' + file + '  \n')
       else:
          # st.text('    └── ' + file)
-         text = text + ('    └── ' + file + '\n')
+         text = text + ('    └── ' + file + '  \n')
 
    text = ''.join(text)
    return text
